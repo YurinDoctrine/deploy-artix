703f4fc4ca4219943809187906c8cdfd53cebbde
YurinDoctrine <bayramyurgidenn@gmail.com>
2023-07-21 21:23:30 +0300

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -19,11 +19,11 @@ echo -e "Load keymap (e.g. us): " && read -p $"> " MY_KEYMAP && sudo loadkeys $M
 
 # Check MY_INIT
 case "$(readlink -f /sbin/init)" in
-*openrc*)
+*"openrc"*)
   MY_INIT="openrc"
   echo -e "Init system ("$MY_INIT"): "
   ;;
-*runit*)
+*"runit"*)
   MY_INIT="runit"
   echo -e "Init system ("$MY_INIT"): "
   ;;
@@ -32,7 +32,7 @@ esac
 # Choose disk
 while :; do
   echo ""
-  sudo fdisk -l
+  sudo sfdisk -l
   echo -e "WARNING: the selected disk will be rewritten."
   echo -e "Disk to install to (e.g. /dev/Xda): " && read -p $"> " MY_DISK
   [ -b "$MY_DISK" ] && break
@@ -41,20 +41,12 @@ done
 
 PART1="$MY_DISK"1
 PART2="$MY_DISK"2
-PART3="$MY_DISK"3
 case "$MY_DISK" in
 *"nvme"*)
   PART1="$MY_DISK"p1
   PART2="$MY_DISK"p2
-  PART3="$MY_DISK"p3
   ;;
 esac
-
-# Swap size
-until (echo "$SWAP_SIZE" | grep -Eq "^[0-9]+$") && [ "$SWAP_SIZE" -gt 0 ] && [ "$SWAP_SIZE" -lt 97 ]; do
-  echo -e "Size of swap partition in GiB (Default: 4GB): " && read -p $"> " SWAP_SIZE
-  [ ! "$SWAP_SIZE" ] && SWAP_SIZE=4
-done
 
 # Choose filesystem
 until [ "$MY_FS"="btrfs" ] || [ "$MY_FS"="ext4" ]; do
@@ -62,7 +54,7 @@ until [ "$MY_FS"="btrfs" ] || [ "$MY_FS"="ext4" ]; do
   [ ! "$MY_FS" ] && MY_FS="ext4"
 done
 
-ROOT_PART=$PART3
+ROOT_PART=$PART2
 [ "$MY_FS"="ext4" ] && ROOT_PART=$PART2
 
 # Encrypt
@@ -71,15 +63,12 @@ echo -e "Encrypt? (y/N): " && read -p $"> " ENCRYPTED
 
 # Layout
 MY_ROOT="/dev/mapper/root"
-MY_SWAP="/dev/mapper/swap"
 if [ "$ENCRYPTED"="y" ]; then
   CRYPTPASS=$(confirm_password "Encryption Password: ")
 else
-  MY_ROOT=$PART3
-  MY_SWAP=$PART2
+  MY_ROOT=$PART2
   [ "$MY_FS"="ext4" ] && MY_ROOT=$PART2
 fi
-[ "$MY_FS"="ext4" ] && MY_SWAP="/dev/MyVolGrp/swap"
 
 # Timezone
 until [ -f /usr/share/zoneinfo/"$REGION_CITY" ]; do
@@ -97,8 +86,8 @@ done
 ROOT_PASSWORD=$(confirm_password "Root Password: ")
 
 installvars() {
-  echo -e MY_INIT="$MY_INIT" MY_DISK="$MY_DISK" PART1="$PART1" PART2="$PART2" PART3="$PART3" \
-    SWAP_SIZE="$SWAP_SIZE" MY_FS="$MY_FS" ROOT_PART="$ROOT_PART" ENCRYPTED="$ENCRYPTED" MY_ROOT="$MY_ROOT" MY_SWAP="$MY_SWAP" \
+  echo -e MY_INIT="$MY_INIT" MY_DISK="$MY_DISK" PART1="$PART1" PART2="$PART2" \
+    MY_FS="$MY_FS" ROOT_PART="$ROOT_PART" ENCRYPTED="$ENCRYPTED" MY_ROOT="$MY_ROOT" \
     REGION_CITY="$REGION_CITY" MY_HOSTNAME="$MY_HOSTNAME" \
     CRYPTPASS="$CRYPTPASS" ROOT_PASSWORD="$ROOT_PASSWORD"
 }
@@ -108,14 +97,13 @@ echo -e "Done with configuration. Installing..."
 # Partition disk
 if [ "$MY_FS"="ext4" ]; then
   layout=",,V"
-  fs_pkgs="lvm2 lvm2-$MY_INIT"
 elif [ "$MY_FS"="btrfs" ]; then
-  layout=",${SWAP_SIZE}G,S\n,,"
+  layout=",,S\n,,"
   fs_pkgs="btrfs-progs"
 fi
 [ "$ENCRYPTED"="y" ] && fs_pkgs=$fs_pkgs+" cryptsetup cryptsetup-$MY_INIT"
 
-printf "label: gpt\n,550M,U\n%s\n" "$layout" | sudo sfdisk "$MY_DISK"
+printf "label: gpt\n,512M,U\n%s\n" "$layout" | sudo sfdisk "$MY_DISK"
 
 # Format and mount partitions
 if [ "$ENCRYPTED"="y" ]; then
@@ -131,17 +119,11 @@ fi
 sudo mkfs.fat -F 32 "$PART1"
 
 if [ "$MY_FS"="ext4" ]; then
-# Setup LVM
-  sudo pvcreate "$MY_ROOT"
-  sudo vgcreate MyVolGrp "$MY_ROOT"
-  sudo lvcreate -L "$SWAP_SIZE"G MyVolGrp -n swap
-  sudo lvcreate -l 100%FREE MyVolGrp -n root
-
-  sudo mkfs.ext4 /dev/MyVolGrp/root
+  sudo mkfs.ext4 -L ROOT "$MY_ROOT"
 
   sudo mount /dev/MyVolGrp/root /mnt
 elif [ "$MY_FS"="btrfs" ]; then
-  sudo mkfs.btrfs "$MY_ROOT"
+  sudo mkfs.btrfs -L "$MY_ROOT"
 
   # Create subvolumes
   sudo mount "$MY_ROOT" /mnt
@@ -155,7 +137,6 @@ elif [ "$MY_FS"="btrfs" ]; then
   sudo mount -t btrfs -o compress=zstd,subvol=home "$MY_ROOT" /mnt/home
 fi
 
-sudo mkswap "$MY_SWAP"
 sudo mkdir /mnt/boot
 sudo mount "$PART1" /mnt/boot
 
