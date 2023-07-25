0e3aa6644f267f06a28059eda26f8f9be01a55ca
Yurin Doctrine <doctriney@protonmail.com>
2023-07-25 14:10:21 +0300

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ fi
 confirm_password() {
   stty -echo
   until [ "$pass1" = "$pass2" ] && [ "$pass2" ]; do
-    printf "\n%s: \n" "$1" >&2 && read -p $"> " pass1
-    printf "\nRe-type %s: \n" "$1" >&2 && read -p $"> " pass2
+    printf "\n%s\n" "$1" >&2 && read -p $"> " pass1
+    printf "Re-type %s\n" "$1" >&2 && read -p $"> " pass2
   done
   stty echo
   echo -e "$pass2"
