afcb440382d9e94fd6a82ba96c80da331cf1ae7f
Yurin Doctrine <doctriney@protonmail.com>
2023-09-23 13:43:55 +0300

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,6 @@ echo -e "LANG=en_GB.UTF-8" >/etc/locale.conf
 echo -e "FONT=ter-v22b
 FONT_MAP=8859-2" >/etc/vconsole.conf
 
-mkdir -p /etc/profile.d
-echo -e "exec setxkbmap $MY_KEYMAP &" >/etc/profile.d/keymap.sh
-
 # Host stuff
 echo -e "$MY_HOSTNAME" >/etc/hostname
 mkdir -p /etc/conf.d
