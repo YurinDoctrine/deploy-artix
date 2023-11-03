9236bec57ff5183be6f552d6e9b07e6334a0eae9
Yurin Doctrine <doctriney@protonmail.com>
2023-11-03 08:29:38 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -471,6 +471,8 @@ echo -e "session required pam_limits.so" >>/etc/pam.d/common-session-noninteract
 
 sed -i -e 's|022|027|g' /etc/login.defs
 
+echo -e "#" >/etc/securetty
+
 echo -e "noarp" >>/etc/dhcpcd.conf
 
 if [ "$MY_INIT" = "openrc" ]; then
