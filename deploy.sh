4c9bd8c91b5349dc4745d1ff36b580fe085df185
Yurin Doctrine <doctriney@protonmail.com>
2023-11-11 13:22:38 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -503,7 +503,9 @@ tee /etc/issue <<"EOF"
   'ooooi:'`                `'';ioxxo'
  'i:'`                          '':io'
 '`                                   `'
+
 \s \r (\m) (\l) \t \d
+
 EOF
 
 if [ "$MY_INIT" = "openrc" ]; then
