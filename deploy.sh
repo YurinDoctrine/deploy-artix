c3d67956bd73c37d51311a78fa5c3c4fb9bf9da5
Yurin Doctrine <doctriney@protonmail.com>
2024-07-10 00:34:12 +0300

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,10 @@ yes "$ROOT_PASSWORD" | passwd $MY_USERNAME
 yes "$ROOT_PASSWORD" | passwd
 
 # Pacman
-sed -i -e 's/Server = http:/#Server = http:/' /etc/pacman.d/mirrorlist
+echo -e '# Default mirrors
+Server = https://mirrors.dotsrc.org/artix-linux/repos/$repo/os/$arch
+#Server = https://mirror.clarkson.edu/artix-linux/repos/$repo/os/$arch
+#Server = http://ftp.ntua.gr/pub/linux/artix-linux/$repo/os/$arch' >/etc/pacman.d/mirrorlist
 sed -i -e s"/\#VerbosePkgLists/VerbosePkgLists/"g /etc/pacman.conf
 sed -i -e s"/\#ParallelDownloads.*/ParallelDownloads = 3/"g /etc/pacman.conf
 sed -i -e s"/\#CheckSpace/CheckSpace/"g /etc/pacman.conf
