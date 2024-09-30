f4a370f8b1b13f5dfa6d32d20c39137b9ff3aca4
Yurin Doctrine <doctriney@protonmail.com>
2024-09-30 19:24:14 +0300

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -429,6 +429,7 @@ fs.protected_symlinks=1
 fs.protected_fifos=2
 fs.protected_regular=2
 fs.suid_dumpable=0
+abi.vsyscall32=0
 kernel.unprivileged_bpf_disabled=1
 kernel.dmesg_restrict=1
 net.ipv6.conf.default.disable_ipv6=1
